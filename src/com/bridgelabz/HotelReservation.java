ef801db929fcc930d04a514af64704119e5a0a19
niludarekar1 <nilu.darekar1@gmail.com>
2022-06-03 12:59:48 +0530

    Modified:Weekend Rate Added:

--- src/com/bridgelabz/HotelReservation.java
+++ src/com/bridgelabz/HotelReservation.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 public class HotelReservation {
     ArrayList<HotelData> hotelDataArrayList = new ArrayList<>();
+    Date date1,date2;
     public void addHotel(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Customer Type :");
@@ -22,12 +23,15 @@ public class HotelReservation {
         String startDate = sc.next();
         System.out.println("Enter the Ending Date :");
         String endDate = sc.next();
-
+        System.out.println("Enter the WeekDay Rate :");
+        int weekdayRate = sc.nextInt();
+        System.out.println("Enter the Weekend Rate :");
+        int weekendRate = sc.nextInt();
         try {
             SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
-            Date date1 = format.parse(startDate);
+            date1 = format.parse(startDate);
             SimpleDateFormat format1 = new SimpleDateFormat("dd-MMM-yyyy");
-            Date date2 = format1.parse(endDate);
+            date2 = format1.parse(endDate);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -35,7 +39,7 @@ public class HotelReservation {
         System.out.println("Enter the Regular Customer Rate :");
         int regularCutomerRate = sc.nextInt();
 
-        HotelData hotelData = new HotelData(customerType,hotelName,rating,regularCutomerRate,startDate,endDate);
+        HotelData hotelData = new HotelData(customerType,hotelName,rating,regularCutomerRate,startDate,endDate,weekdayRate,weekendRate);
         hotelDataArrayList.add(hotelData);
     }
 
@@ -43,7 +47,16 @@ public class HotelReservation {
         System.out.println(hotelDataArrayList);
     }
 
-    public void cheapestHotel() {
+    public void cheapestHotel(String date1, String date2) {
+
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
+            Date strDate = format.parse(date1);
+            SimpleDateFormat format1 = new SimpleDateFormat("dd-MMM-yyyy");
+            Date endDate = format1.parse(date2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         List<HotelData> minHotelData = hotelDataArrayList.stream().filter(x -> x.getRegularCustomerRate()==
         hotelDataArrayList.stream().map(HotelData::getRegularCustomerRate).sorted().findFirst().orElse(null))
                 .collect(Collectors.toList());
--- src/com/bridgelabz/HotelReservationSystem.java
+++ src/com/bridgelabz/HotelReservationSystem.java
@@ -1,15 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class HotelReservationSystem {
     public static void main(String[] args) {
         //Welcome message for User
         System.out.println("Welcome to Hotel Reservation System Problem.");
+
+        Scanner sc = new Scanner(System.in);
 
         HotelReservation hotelReservation = new HotelReservation();
         hotelReservation.addHotel();
         hotelReservation.addHotel();
         hotelReservation.addHotel();
         hotelReservation.printHotel();
-        hotelReservation.cheapestHotel();
+        System.out.println("Enter the Starting Date :");
+        String date1 = sc.next();
+        System.out.println("Enter the Ending Date :");
+        String date2 = sc.next();
+        hotelReservation.cheapestHotel(date1,date2);
     }
 }
